644c1a131456bcba5a8320b1caf059c87f083baa
harrykum <harrykumar9211@gmail.com>
2022-10-05 03:52:00 +0530

    first commit

---
+++ Automation/src/A.java
@@ -0,0 +1,15 @@
+package qsp;
+
+import org.openqa.selenium.WebDriver;
+
+public class A {
+	static void testA(WebDriver driver) {
+		driver.get("https:www.google.com");
+		String title=driver.getTitle();
+		System.out.println(title);
+		driver.close();
+	}
+
+	
+
+}
